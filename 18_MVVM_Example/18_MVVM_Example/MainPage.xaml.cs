03cb95157df31e2996a7aa27bb099bdee664f0bf
Bernat <bernat.orellana@gmail.com>
2018-12-05 14:59:05 +0100

    MVVM intro

---
+++ 18_MVVM_Example/18_MVVM_Example/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using _18_MVVM_Example.ViewModels;
+using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Globalization;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace _18_MVVM_Example
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+
+
+            this.InitializeComponent();
+
+            this.DataContext = new MainPageViewModel();
+
+            Messenger.Default.Register<ShowMessageDialog>
+            (
+                this,
+                (action) => ReceiveMessage(action)
+            );
+        }
+
+        private async void ReceiveMessage(ShowMessageDialog action)
+        {
+            var messageDialog = new MessageDialog(action.Message);
+
+            await messageDialog.ShowAsync();
+
+        }
+
+        private void cboIdiomes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Canvi programàtic d'idioma
+            //-------------------------------------------------
+            ApplicationLanguages.PrimaryLanguageOverride =
+                cboIdiomes.SelectedValue.ToString();
+            
+            this.Frame.Navigate(typeof(MainPage));            
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            cboIdiomes.ItemsSource = 
+                new List<string> { "ca-es", "es-es", "en-us" };
+        }
+    }
+}
---
+++ 18_MVVM_Example/18_MVVM_Example/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,59 @@
+﻿using _18_MVVM_Example.Models;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_MVVM_Example.ViewModels
+{
+    class MainPageViewModel:INotifyPropertyChanged
+    {
+
+        public MainPageViewModel()
+        {
+            Persona p = Persona.GetPersones()[0];
+            this.nom = p.Nom;
+            this.edat = p.Edat + "";
+            this.actiu = p.Actiu;
+            this.esHome = p.Sexe;
+        }
+
+        string nom;
+        string edat;
+        bool actiu;
+        bool esHome;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Nom { get => nom; set => nom = value; }
+        public string Edat { get => edat; set => edat = value; }
+        public bool Actiu { get => actiu; set => actiu = value; }
+        public bool EsHome { get => esHome; set => esHome = value; }
+
+
+      
+
+
+
+
+
+
+
+
+        /*
+        private RelayCommand _showMessageCommand;
+
+        public RelayCommand ShowMessageCommand => _showMessageCommand ?? (_showMessageCommand = new RelayCommand(ShowMessage));
+
+
+        private void ShowMessage()
+        {
+            var msg = new ShowMessageDialog { Message = "Hello World" };
+            Messenger.Default.Send<ShowMessageDialog>(msg);
+        }*/
+    }
+}
