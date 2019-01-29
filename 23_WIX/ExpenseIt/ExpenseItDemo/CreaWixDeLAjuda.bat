0e296b6d8182d5b8493b78ffff6816ddd1d3f3e2
Bernat <bernat.orellana@gmail.com>
2019-01-29 20:20:28 +0100

    WIX amb integració amb HEAT (PreBuild Target)

---
+++ 23_WIX/ExpenseIt/ExpenseItDemo/CreaWixDeLAjuda.bat
@@ -0,0 +1,3 @@
+"c:\Program Files (x86)\WiX Toolset v3.11\bin\heat.exe" dir .\help -dr DIR_HELPFILES -cg CG_DocumentationFiles -gg -g1 -sf -srd -var "var.PathDocumentacio" -out ..\ExpenseItDemo.SetupProject\DocumentationFiles.wxs
+
+pause
